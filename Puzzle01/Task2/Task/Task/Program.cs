9327d72cbe6f9b5842a0ca0cf3b9678e528cfefb
Mate Sosa <mate.sosa@pakel.hr>
2023-05-02 14:37:57 +0200

    Day one

---
+++ Puzzle01/Task2/Task/Task/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+
+            var lines = File.ReadAllLines("input.txt");
+            var counter = 0;
+            var list = new List<int>();
+
+            var elf = 0;
+
+            foreach (var line in lines)
+            {
+
+
+                if (line == String.Empty)
+                {
+                    list.Add(elf);
+                    elf = 0;
+                }
+                else
+                {
+                    elf += Int32.Parse(line);
+                }
+
+            }
+
+
+            var maxElf = list.OrderByDescending(o => o).Take(3).Sum();
+
+
+            Console.WriteLine("Result is {0}", maxElf);
+            Console.ReadKey();
+
+        }
+    }
+}

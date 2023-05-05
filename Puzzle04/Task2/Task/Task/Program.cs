f52f5b0bd8efb29e20ab34f18fd447e7f8c95e37
Mate Sosa <mate.sosa@pakel.hr>
2023-05-05 14:43:45 +0200

    Puzzle 04

---
+++ Puzzle04/Task2/Task/Task/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+            var counter = 0;
+            var list = new List<int>();
+
+            var exceptionList = new List<Int32>();
+
+
+            foreach (var line in lines)
+            {
+                var splits = line.Split(",");
+                var firstToken = splits[0];
+                var secondToken = splits[1];
+
+                var splitsFirst = firstToken.Split("-");
+                var splitsSecond = secondToken.Split("-");
+
+                var ffone = Int32.Parse(splitsFirst[0]);
+                var fftwo = Int32.Parse(splitsFirst[1]);
+
+                var ssone = Int32.Parse(splitsSecond[0]);
+                var sstwo = Int32.Parse(splitsSecond[1]);
+
+                var firstRange  = Enumerable.Range(ffone, fftwo - ffone + 1).ToList();
+                var secondRange =Enumerable.Range(ssone, sstwo - ssone +1).ToList();
+
+                if (firstRange.Intersect(secondRange).ToList().Count > 0) 
+                    counter++;
+
+            }
+
+            Console.WriteLine("Result is {0}", counter);
+            Console.ReadKey();
+        }
+    }
+}

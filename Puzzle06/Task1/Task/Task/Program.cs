c6d555b8ae03e667295cf6bbab00704c9539045c
Mate Sosa <mate.sosa@pakel.hr>
2023-05-10 12:54:29 +0200

    Puzzle 6 fix

---
+++ Puzzle06/Task1/Task/Task/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Task
+{
+    class Program
+    {
+        
+        public static List<char> _slidingWindow = new List<char>();
+        public static int _counter = 0;
+
+        static void Main(string[] args)
+        {
+
+            var lines = File.ReadAllLines("input.txt");
+            var firstline = lines.First();
+
+            for (int i = 3; i < firstline.Length; i++)
+            {
+                _slidingWindow.Clear();
+
+                for (int j = 3; j >= 0; j--)
+                {
+
+                    _slidingWindow.Add(firstline[i - j]);
+                   
+                }
+
+                if (_slidingWindow.Distinct().Count() == 4)
+                {
+                    _counter = i;
+                    break;
+                }
+                
+            }
+
+           
+            
+            Console.WriteLine("Result is {0}", _counter+1);
+            Console.ReadKey();
+        }
+
+       
+    }
+}

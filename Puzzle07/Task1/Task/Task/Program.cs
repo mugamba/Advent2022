ea15200aa25751f3dfeb52cba26202de1bbeb068
Mate Sosa <mate.sosa@pakel.hr>
2023-05-10 16:22:52 +0200

    Task 7

---
+++ Puzzle07/Task1/Task/Task/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Task
+{
+    class Program
+    {
+        public static Dir _tree = null;
+        public static Dir _currentDir = null;
+        public static long _globalsum = 0;
+
+
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+            
+            
+            foreach (var line in lines)
+            {
+                if (line.Contains("$ cd"))
+                {
+
+                    var split = line.Split(" ");
+                    var cdfunc = split[2];
+
+                    if (cdfunc == "..")
+                    {
+                        _currentDir = _currentDir.Parent;
+                    }
+                    else
+                    {
+                        var temp = new Dir();
+                        temp.Name = cdfunc;
+                        if (cdfunc == "/")
+                        {
+                            _tree = temp;
+                            temp.Parent = null;
+                            _currentDir = temp;
+                        }
+                        else
+                        {
+                            temp.Parent = _currentDir;
+                            _currentDir.Dirs.Add(temp);
+                            _currentDir = temp;
+                        }
+                    }
+                }
+
+                if (!(line.StartsWith("$") || line.StartsWith("dir")))
+                {
+                    var filesize = line.Split(" ");
+                    _currentDir.Files.Add(long.Parse(filesize[0]));
+                }
+
+            }
+            TraverseTree(_tree);
+
+            Console.WriteLine("Result is {0}", _globalsum);
+            Console.ReadKey(); 
+        }
+
+        public static void TraverseTree(Dir current)
+        {
+            if (current.DirSize <= 100000)
+            {
+                _globalsum += current.DirSize;
+            }
+
+            foreach (var child in current.Dirs)
+            {
+                TraverseTree(child);
+            }
+        }
+    }
+
+
+    
+
+
+    public class Dir
+    {
+
+        public Dir()
+        {
+            Dirs = new List<Dir>();
+            Files = new List<long>(); 
+        }
+
+        public String Name { get; set; }
+        public List<Dir> Dirs { get; set; }
+        public List<long> Files { get; set; }
+        public Dir Parent { get; set; }
+
+        public long DirSize
+        {
+            get { return (Files == null ? 0 : Files.Sum()) + (Dirs == null ? 0 : Dirs.Select(o => o.DirSize).Sum()); }
+        }
+    }
+}
---
+++ Puzzle07/Task2/Task/Task/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Task
+{
+    class Program
+    {
+        public static Dir _tree = null;
+        public static Dir _currentDir = null;
+        public static long _globalsum = 0;
+        public static List<long> _matchingDirectories = new List<long>();
+
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+            
+            
+            foreach (var line in lines)
+            {
+                if (line.Contains("$ cd"))
+                {
+
+                    var split = line.Split(" ");
+                    var cdfunc = split[2];
+
+                    if (cdfunc == "..")
+                    {
+                        _currentDir = _currentDir.Parent;
+                    }
+                    else
+                    {
+                        var temp = new Dir();
+                        temp.Name = cdfunc;
+                        if (cdfunc == "/")
+                        {
+                            _tree = temp;
+                            temp.Parent = null;
+                            _currentDir = temp;
+                        }
+                        else
+                        {
+                            temp.Parent = _currentDir;
+                            _currentDir.Dirs.Add(temp);
+                            _currentDir = temp;
+                        }
+                    }
+                }
+
+                if (!(line.StartsWith("$") || line.StartsWith("dir")))
+                {
+                    var filesize = line.Split(" ");
+                    _currentDir.Files.Add(long.Parse(filesize[0]));
+                }
+
+            }
+
+            var unusedSpace = 70000000 - _tree.DirSize;
+            var minimumSizeTodelete =  (long)30000000 - unusedSpace;
+
+            FindDirectory(_tree, minimumSizeTodelete);
+    
+            Console.WriteLine("Result is {0}", _matchingDirectories.Min());
+            Console.ReadKey(); 
+        }
+
+        public static void FindDirectory(Dir current,long minimumSizeTodelete)
+        {
+            if (current.DirSize >= minimumSizeTodelete)
+            {
+                _matchingDirectories.Add(current.DirSize);
+            }
+
+            foreach (var child in current.Dirs)
+            {
+                FindDirectory(child, minimumSizeTodelete);
+            }
+        }
+    }
+
+
+    
+
+
+    public class Dir
+    {
+        public Dir()
+        {
+            Dirs = new List<Dir>();
+            Files = new List<long>(); 
+        }
+
+        public String Name { get; set; }
+        public List<Dir> Dirs { get; set; }
+        public List<long> Files { get; set; }
+        public Dir Parent { get; set; }
+
+        public long DirSize
+        {
+            get { return (Files == null ? 0 : Files.Sum()) + (Dirs == null ? 0 : Dirs.Select(o => o.DirSize).Sum()); }
+        }
+    }
+}
